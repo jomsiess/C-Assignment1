8d7ae990668dfd9db9a5a8806d65f4382a686cd1
jomsiess <jomsiess@gmail.com>
2020-10-06 13:49:57 -0500

    adding files

---
+++ TechJobsConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechJobsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
